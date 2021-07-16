9ca11fbe2f7d3bf2656f49f650eff4b9b3a6249d
KOUASSI CHARLES OTH-NIEL <kouassicharlesothniel@gmail.com>
2021-07-16 23:53:38 +0000

    modified works

--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -3,23 +3,23 @@ import "./style.css";
 const About = () => {
   return (
     <div className="works">
-      <h1>Experiences</h1>
-      <p>Company: Akil Technologie</p>
-      <p>Role: Développeur FrontEnd (Web & Mobile ) </p>
-      <p>Date: 2020 - on going</p>
+      <h1>Expériences</h1>
+      <p>Compagnie: Akil Technologie</p>
+      <p>Rôle: Développeur FrontEnd (Web & Mobile) </p>
+      <p>Date: 2020 - en cours</p>
       <p>
         Conception et architecture de l'application basée sur une solution React  <br />
-        Developpement de composants React  <br />
+        Développement de composants React  <br />
         Création de services pour communiquer avec des API  <br />
         Gestion d'accès aux pages et erreurs techniques  <br />
       </p>
       <br />
-      <p>Company: Ivoire Geek School (IGS)</p>
-      <p>Role: Stagiaire en développement Web/Mobile</p>
+      <p>Compagnie: Ivoire Geek School (IGS)</p>
+      <p>Rôle: Stagiaire en développement Web/Mobile</p>
       <p>Date: 2019 - 2020</p>
       <p>
         Développeur front-end de sites internet sous WordPress et React <br />
-        Webdesign (maquette, schemas conceptuels)  <br />
+        Webdesign (maquettes, schemas conceptuels)  <br />
         Relation avec les clients
       </p>
     </div>
